19ee2b449f28395034229a3ed4aec32c3ef6d2b1
sora0no0ningyo <sora0no0ningyo@gmail.com>
2019-04-19 04:24:14 +0200

    viewing+addtion+validation

--- main-files/es6/scriptsheet.js
+++ main-files/es6/scriptsheet.js
@@ -1,7 +1,14 @@
-/*global console,document,alert,$,window */
+/*global console,document,alert,$,window,setTimeout */
 /*jslint es6 */
-
-  // Initialize Firebase
+'use strict';
+// ...........
+$(function () {
+    $('input[type="text"]').tooltip({
+        trigger: 'manual'
+    });
+});
+// ...........
+// Initialize Firebase
 var config = {
     apiKey: "AIzaSyCe1QexEQjRbiM-1zubU2qXJAzn1tgdvfI",
     authDomain: "fire-cafe-f1.firebaseapp.com",
@@ -12,10 +19,84 @@ var config = {
 };
 firebase.initializeApp(config);
 const dataBase = firebase.firestore();
-dataBase.settings({timestampsInSnapshots: true});
+dataBase.settings({
+    timestampsInSnapshots: true
+});
 
-    // Start Project
+// Start Project
+
+// Viewing Docs
+
+const cafeList = document.querySelector('#cafe-list');
+
+    // Rendercafe Function
+
+function renderCafe(doc) {
+    let li = document.createElement('li'),
+        name = document.createElement('span'),
+        city = document.createElement('span'),
+        price = document.createElement('span');
+
+    li.setAttribute('data-id', doc.id);
+    name.textContent = doc.data().Coffee;
+    city.textContent = doc.data().City;
+    price.textContent = doc.data().Price;
+
+    li.appendChild(name);
+    li.appendChild(city);
+    li.appendChild(price);
+
+    cafeList.appendChild(li);
+}
+
+// Get Collections
+
 dataBase.collection('Coffees').get().then((snapshot) => {
     'use strict';
-    console.log(snapshot.docs);
+    snapshot.docs.forEach((doc) => {
+        renderCafe(doc);
+    });
+});
+
+// Send Collections
+const newCoffee = document.getElementById('add-cafe-form');
+newCoffee.addEventListener('submit', function (e) {
+
+
+    e.preventDefault();
+    if (newCoffee.Coffee.value === '' || newCoffee.City.value === '' || newCoffee.Price.value === ''){
+
+        if (newCoffee.Coffee.value === '') {
+
+
+            $('input[name="Coffee"]').tooltip('show');
+
+            setTimeout(() => {
+
+                $('input[name="Coffee"]').tooltip('hide');
+
+            }, 3000);
+        }
+        if (newCoffee.City.value === '') {
+            $('input[name="City"]').tooltip('show');
+            setTimeout(() => {
+                $('input[name="City"]').tooltip('hide');
+            }, 3000);
+        }
+        if (newCoffee.Price.value === '') {
+            $('input[name="Price"]').tooltip('show');
+            setTimeout(() => {
+                $('input[name="Price"]').tooltip('hide');
+            }, 3000);
+        }
+    } else {
+        dataBase.collection('Coffees').add({
+            Coffee: newCoffee.Coffee.value,
+            City: newCoffee.City.value,
+            Price: newCoffee.Price.value
+        });
+    }
+    newCoffee.Coffee.value = '';
+    newCoffee.City.value = '';
+    newCoffee.Price.value = '';
 });
